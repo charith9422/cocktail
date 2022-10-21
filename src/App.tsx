eb04f698c0089892e7593310aef559619155d7ad
charith <charith9422@gmail.com>
2022-10-21 12:25:23 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { DatePicker } from "antd";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { t } = useTranslation();
+	return (
+		<div className="App">
+			<button className="btn btn-primary">Test</button>
+			<DatePicker />
+			<p>{t("test")}</p>
+		</div>
+	);
 }
 
 export default App;
